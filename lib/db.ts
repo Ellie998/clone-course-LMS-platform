5d5b65fd5ddbbac0314766c69d135dcc5193ff7b
Ellie <dpfla9144@naver.com>
2023-11-21 16:28:00 +0900

    add comment

--- lib/db.ts
+++ lib/db.ts
@@ -6,6 +6,7 @@ declare global {
 
 // globalThis는 전역객체에 접근할 수 있는 식별자
 export const db = globalThis.prisma || new PrismaClient();
+// use `prisma` in your application to read and write data in your DB
 
 if (process.env.NODE_ENV !== "production") globalThis.prisma = db;
 
