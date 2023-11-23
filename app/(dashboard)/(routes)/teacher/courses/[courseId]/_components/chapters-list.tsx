266cbd21d0ae9d441f7b90dee5a8b93d4b49398d
Ellie <dpfla9144@naver.com>
2023-11-23 14:06:49 +0900

    add ChaptersList basic codes

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/chapters-list.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/chapters-list.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { Chapter } from "@prisma/client";
+import { useEffect, useState } from "react";
 
 interface ChaptersListProps {
   items: Chapter[];
@@ -9,6 +10,23 @@ interface ChaptersListProps {
 }
 
 const ChaptersList = ({ items, onReorder, onEdit }: ChaptersListProps) => {
+  const [isMounted, setIsMounted] = useState(false);
+  const [chapters, setChapters] = useState(items);
+
+  //  code for preventing hydation mismatch between client side and server side
+  // this component is only rendered client side
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+  //rehydrate
+  useEffect(() => {
+    setChapters(items);
+  }, [items]);
+
+  if (!isMounted) {
+    return null;
+  }
+
   return <div>Chapter List</div>;
 };
 
