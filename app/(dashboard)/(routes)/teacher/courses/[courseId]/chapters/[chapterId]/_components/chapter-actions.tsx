a86c28dea8d5aef82a433ca15e960bc2225f8f5c
Ellie <dpfla9144@naver.com>
2023-11-24 23:18:42 +0900

    add onDelete function in ChapterActions

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/_components/chapter-actions.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/_components/chapter-actions.tsx
@@ -1,8 +1,13 @@
 "use client";
 
+import { useState } from "react";
+import toast from "react-hot-toast";
+
 import ConfirmModal from "@/components/modals/confirm-modal";
 import { Button } from "@/components/ui/button";
 import { Trash } from "lucide-react";
+import axios from "axios";
+import { useRouter } from "next/navigation";
 
 interface ChapterActionProps {
   disable: boolean;
@@ -17,17 +22,37 @@ const ChapterActions = ({
   chapterId,
   isPublished,
 }: ChapterActionProps) => {
+  const router = useRouter();
+
+  const [isLoading, setIsLoading] = useState(false);
+
+  const onDelete = async () => {
+    try {
+      setIsLoading(true);
+
+      await axios.delete(`/api/courses/${courseId}/chapters/${chapterId}`);
+
+      toast.success("Chapter deleted");
+      router.refresh();
+      router.push(`/teacher/courses/${courseId}`);
+    } catch (error) {
+      toast.error("Something went wrong");
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
   return (
     <div className="flex items-center gap-x-2">
       <Button
         onClick={() => {}}
-        disabled={disable}
+        disabled={disable || isLoading}
         variant={"outline"}
         size={"sm"}>
         {isPublished ? "Unpublish" : "Publish"}
       </Button>
-      <ConfirmModal onConfirm={() => {}}>
-        <Button>
+      <ConfirmModal onConfirm={onDelete}>
+        <Button disabled={isLoading}>
           <Trash size={"sm"} className="w-4 h-4" />
         </Button>
       </ConfirmModal>
