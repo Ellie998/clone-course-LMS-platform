84b01e95ffe03a28d41545f9663fca963245ba89
Ellie <dpfla9144@naver.com>
2023-11-26 15:53:08 +0900

    get course using getCourse action

--- app/(dashboard)/(routes)/search/page.tsx
+++ app/(dashboard)/(routes)/search/page.tsx
@@ -1,13 +1,31 @@
 import { db } from "@/lib/db";
+import { getCourses } from "@/actions/get-courses";
 import Categories from "./_components/categories";
 import SearchInput from "@/components/seach-input";
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
 
-const SearchPage = async () => {
+interface SearchPageProps {
+  searchParams: {
+    title: string;
+    categoryId: string;
+  };
+}
+
+const SearchPage = async ({ searchParams }: SearchPageProps) => {
+  const { userId } = auth();
+
+  if (!userId) {
+    return redirect("/");
+  }
+
   const categories = await db.category.findMany({
     orderBy: {
       name: "asc",
     },
   });
+
+  const course = await getCourses({ userId, ...searchParams });
   return (
     <>
       <div className="block px-6 pt-6 md:hidden md:mb-0">
