b371e2ebafd34e30f7680bf1b589fd9fdf338418
Ellie <dpfla9144@naver.com>
2023-11-26 12:51:54 +0900

    change datatable code - using our data

--- app/(dashboard)/(routes)/teacher/courses/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/page.tsx
@@ -1,28 +1,31 @@
-import { Button } from "@/components/ui/button";
+import { redirect } from "next/navigation";
+import { auth } from "@clerk/nextjs";
+
 import Link from "next/link";
 import { DataTable } from "./__components/data-table";
 import { columns } from "./__components/colums";
-
-async function getData(): Promise<any[]> {
-  // Fetch data from your API here.
-  return [
-    {
-      id: "728ed52f",
-      amount: 100,
-      status: "pending",
-      email: "m@example.com",
-    },
-    // ...
-  ];
-}
+import { db } from "@/lib/db";
 
 const CoursesPage = async () => {
-  const data = await getData();
+  const { userId } = auth();
+
+  if (!userId) {
+    return redirect("/");
+  }
+
+  const courses = await db.course.findMany({
+    where: {
+      userId,
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
 
   return (
     <div className="p-6">
       <Link href={"/teacher/create"}>
-        <DataTable columns={columns} data={data} />
+        <DataTable columns={columns} data={courses} />
       </Link>
     </div>
   );
