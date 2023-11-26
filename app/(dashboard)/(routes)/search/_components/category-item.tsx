605d15c60d7e90dadadd24ce982fe4220a218563
Ellie <dpfla9144@naver.com>
2023-11-26 14:18:53 +0900

    add categoryItem onClick feature - url query change and clicked btn style change

--- app/(dashboard)/(routes)/search/_components/category-item.tsx
+++ app/(dashboard)/(routes)/search/_components/category-item.tsx
@@ -1,6 +1,8 @@
 "use client";
 
+import qs from "query-string";
 import { cn } from "@/lib/utils";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
 import { IconType } from "react-icons";
 
 interface CategoryItemProps {
@@ -10,13 +12,37 @@ interface CategoryItemProps {
 }
 
 const CategoryItem = ({ label, value, icon: Icon }: CategoryItemProps) => {
+  const pathname = usePathname();
+  const router = useRouter();
+  const searchParams = useSearchParams();
+
+  const currentCategoryId = searchParams.get("categoryId");
+  const currentTitle = searchParams.get("title");
+
+  const isSeleted = currentCategoryId === value;
+
+  const onClick = () => {
+    const url = qs.stringifyUrl(
+      {
+        url: pathname,
+        query: {
+          title: currentTitle,
+          categoryId: isSeleted ? null : value,
+        },
+      },
+      { skipNull: true, skipEmptyString: true }
+    );
+    router.push(url);
+  };
+
   return (
     <button
       className={cn(
-        "py-2 px-3 text-sm border border-slate-200 rounded-full flex items-center gap-x-1 hover:border-sky-700 transition"
-        // change style if active
+        "py-2 px-3 text-sm border border-slate-200 rounded-full flex items-center gap-x-1 hover:border-sky-700 transition",
+        isSeleted && "border-sly-700 bg-sky-200/20 text-sky-800"
       )}
-      type="button">
+      type="button"
+      onClick={onClick}>
       {Icon && <Icon size={20} />}
       <div className="truncate">{label}</div>
     </button>
