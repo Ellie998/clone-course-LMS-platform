9249b3bff167333aece2b648f129c2fb4d20bfe8
Ellie <dpfla9144@naver.com>
2023-11-26 14:39:15 +0900

    add styles code in searchInput component

--- components/seach-input.tsx
+++ components/seach-input.tsx
@@ -1,7 +1,18 @@
 "use client";
 
+import { Search } from "lucide-react";
+import { Input } from "@/components/ui/input";
+
 const SearchInput = () => {
-  return <div>Seach input</div>;
+  return (
+    <div className="relative">
+      <Search className="absolute w-4 h-4 top-3 left-3 text-slate-600" />
+      <Input
+        className="w-full md:w-[300px] pl-9 rounded-full bg-slate-100 focus-visible:ring-slate-200"
+        placeholder="Search for a course"
+      />
+    </div>
+  );
 };
 
 export default SearchInput;
