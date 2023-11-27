7c5022dfd9c367e2997b8bb4bc03a1e50bc2bcf4
Ellie <dpfla9144@naver.com>
2023-11-27 15:17:15 +0900

    change chapterId page code

--- app/(course)/courses/[courseId]/chapters/[chapterId]/page.tsx
+++ app/(course)/courses/[courseId]/chapters/[chapterId]/page.tsx
@@ -1,3 +1,32 @@
-export default function ChapterIdPage() {
-  return <div>Chapter Id Page</div>;
+import { db } from "@/lib/db";
+import { redirect } from "next/navigation";
+
+export default async function ChapterIdPage({
+  params,
+}: {
+  params: {
+    courseId: string;
+  };
+}) {
+  const course = await db.course.findUnique({
+    where: {
+      id: params.courseId,
+    },
+    include: {
+      chapters: {
+        where: {
+          isPublished: true,
+        },
+        orderBy: {
+          position: "asc",
+        },
+      },
+    },
+  });
+
+  if (!course) {
+    return redirect("/");
+  }
+
+  return redirect(`/courses/${course.id}/chapters/${course.chapters[0].id}`);
 }
