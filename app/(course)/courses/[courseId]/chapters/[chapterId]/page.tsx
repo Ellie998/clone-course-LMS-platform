8c60b30de99bf819c0d09cac5f1cbd34e466860c
Ellie <dpfla9144@naver.com>
2023-11-27 15:54:45 +0900

    use getChapter action in chapter-id page

--- app/(course)/courses/[courseId]/chapters/[chapterId]/page.tsx
+++ app/(course)/courses/[courseId]/chapters/[chapterId]/page.tsx
@@ -1,3 +1,5 @@
+import { getChapter } from "@/actions/get-chapter";
+import Banner from "@/components/banner";
 import { auth } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
 
@@ -15,5 +17,41 @@ export default async function ChapterIdPage({
     return redirect("/");
   }
 
-  return;
+  const {
+    chapter,
+    course,
+    muxData,
+    attachments,
+    nextChapter,
+    userProgress,
+    purchase,
+  } = await getChapter({
+    userId,
+    courseId: params.courseId,
+    chapterId: params.chapterId,
+  });
+
+  if (!chapter || !course) {
+    return redirect("/");
+  }
+
+  const isLocked = !chapter.isFree && !purchase;
+  const completeInEnd = !!purchase && !userProgress?.isCompleted;
+
+  return (
+    <div>
+      {userProgress?.isCompleted && (
+        <Banner
+          label={"You already completed this chapter."}
+          variant={"success"}
+        />
+      )}
+      {isLocked && (
+        <Banner
+          label={"You need to purchase this course to watch this chapter."}
+          variant={"warning"}
+        />
+      )}
+    </div>
+  );
 }
