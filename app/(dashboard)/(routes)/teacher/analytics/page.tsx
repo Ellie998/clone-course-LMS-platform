ac7491cf2581ddf6a33725dc5e9f9d28dde4981d
Ellie <dpfla9144@naver.com>
2023-11-28 22:01:14 +0900

    add get-analytics action

--- app/(dashboard)/(routes)/teacher/analytics/page.tsx
+++ app/(dashboard)/(routes)/teacher/analytics/page.tsx
@@ -1,5 +1,24 @@
-const AnalyticsPage = () => {
-  return <div>Analytics Page</div>;
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+import { getAnalytics } from "@/actions/get-analytics";
+
+const AnalyticsPage = async () => {
+  const { userId } = auth();
+
+  if (!userId) {
+    return redirect("/");
+  }
+
+  const { data, totalRevenue, totalSales } = await getAnalytics(userId);
+
+  return (
+    <div className="p-6">
+      <div className="grid grid-cols-1 gap-4 mb-4 md:grid-cols-2">
+        {/* <DataCard/> */}
+      </div>
+    </div>
+  );
 };
 
 export default AnalyticsPage;
